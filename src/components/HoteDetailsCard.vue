193f1828ce404375faab21b055dec68e5393e418
Bricute322 <jbeugenio34@gmail.com>
2023-07-21 17:54:43 +0800

    7/21/20232, MODIFIED: components/HotelDetailsCard pages/HotelDetailsPage.vue

--- src/components/HoteDetailsCard.vue
+++ src/components/HoteDetailsCard.vue
@@ -1,54 +1,50 @@
 <template>
-  <div class="row">
-    <div class="col">
-      <!-- <q-img class="q-pa-md" src="https://cdn.quasar.dev/img/avatar.png" /> -->
-      <q-card style="width: auto; max-width: 1000px; height: auto">
-        <q-card-section horizontal>
-          <q-card-section>
+  <!-- <q-img class="q-pa-md" src="https://cdn.quasar.dev/img/avatar.png" /> -->
+  <q-card style="width: auto; max-width: 1000px; height: auto">
+    <q-card-section horizontal>
+      <!-- <q-card-section>
             <q-img
               style="width: 100px"
               class="q-pa-md"
               src="https://cdn.quasar.dev/img/avatar.png"
-          /></q-card-section>
+          /></q-card-section> -->
+      <q-card-section>
+        <div class="text-h5 q-mt-sm q-mb-xs text-primary">
+          Hotel name - {{ location }}
+        </div>
+        <div class="text-subtitle1">
+          <q-icon name="location_on" />
+          {{ location }}
+        </div>
+        <div class="text-subtitle1" v-if="showFullDescription">
+          <q-icon name="room_service" /> {{ description }}
+        </div>
+        <div v-else>
+          {{ truncatedDescription }}
+          <q-btn @click="showDialog" label="Show More" color="primary" flat />
+        </div>
+      </q-card-section>
+      <q-dialog v-model="dialogVisible">
+        <q-card>
           <q-card-section>
-            <div class="text-h5 q-mt-sm q-mb-xs text-primary">
-              Hotel name - {{ location }}
-            </div>
-            <div class="text-subtitle1">
-              <q-icon name="location_on" />
-              {{ location }}
-            </div>
-            <div class="text-subtitle1" v-if="showFullDescription">
-              <q-icon name="room_service" /> {{ description }}
-            </div>
-            <div v-else>
-              {{ truncatedDescription }}
-              <q-btn
-                @click="showDialog"
-                label="Show More"
-                color="primary"
-                flat
-              />
-            </div>
+            {{ description }}
           </q-card-section>
-          <q-dialog v-model="dialogVisible">
-            <q-card>
-              <q-card-section>
-                {{ description }}
-              </q-card-section>
-              <q-btn @click="closeDialog" label="Close" />
-            </q-card>
-          </q-dialog>
-          <q-card-actions class="flex flex-center q-ml-lg"
-            ><q-btn
-              color="primary"
-              label="Select Rooms"
-              align="between"
-              icon-right="hotel"
-          /></q-card-actions>
-        </q-card-section>
-      </q-card>
-      <!-- <q-card class="q-px-none">
+          <q-btn @click="closeDialog" label="Close" />
+        </q-card>
+      </q-dialog>
+      <q-card-actions class="flex flex-center q-ml-lg"
+        ><q-btn
+          color="primary"
+          label="Select Rooms"
+          align="between"
+          icon-right="hotel"
+      /></q-card-actions>
+    </q-card-section>
+    <q-card-section horizontal>
+      <q-card-section></q-card-section>
+    </q-card-section>
+  </q-card>
+  <!-- <q-card class="q-px-none">
         <img
           class="q-pa-md"
           src="https://cdn.quasar.dev/img/avatar.png"
@@ -59,8 +55,6 @@
             max-width: 200px;
           "
       /></q-card> -->
-    </div>
-  </div>
 </template>
 
 <script>
